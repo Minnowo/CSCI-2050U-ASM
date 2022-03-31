9a43309b782b640afa18e7fc6f221028c44c3db8
minnowo <minnowbrine@live.ca>
2022-03-31 12:59:58 -0400

    lecture 1

---
+++ Lecture/01/main.asm
@@ -0,0 +1,38 @@
+extern printf
+extern exit
+
+global main
+
+section .text
+
+main:
+  ; printf("Hello, world!\n");
+  mov rdi, message
+  mov rax, 0
+  push rbx
+  call printf
+  pop rbx
+
+  ; exit(0)
+  mov rax, 0
+  call exit
+
+section .data
+    message db "Hello, world!", 0ah, 0dh, 0
+    ; db -> byte
+    greeting db "hello", 0 
+    ; dq -> qword -> long
+    count dq 0
+    ; dq -> qword -> long
+    list dq 1,2,3,4,5,6,7,8,9,10
+
+section .bss 
+    
+    ; resB -> b meaning byte 
+    firstName resb 50 ; a byte array of 50
+
+    ; resQ -> q meaning quad (qword -> long)
+    age resq 1 ; a single qword
+
+    ; resQ -> qmeaning quad 
+    grades resq 10 ; a qword array of 10 
