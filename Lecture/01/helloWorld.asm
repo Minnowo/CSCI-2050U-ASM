8df9377cae06b0e50de859fa2741ae5c8fe09fea
minnowo <minnowbrine@live.ca>
2022-03-31 14:02:10 -0400

    lecture 01 done

--- Lecture/01/helloWorld.asm
+++ Lecture/01/helloWorld.asm
@@ -7,6 +7,8 @@ section .text
 
 main:
   ; printf("Hello, world!\n");
+  ; setup for the syscall printf 
+  ; list of sys calls for linux http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
   mov rdi, message
   mov rax, 0
   push rbx
